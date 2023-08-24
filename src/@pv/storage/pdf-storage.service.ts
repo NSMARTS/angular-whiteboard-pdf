d2fe9aefd41331a1bd118dff1d3d61e96b219b92
limhokyun <lim@nsmarts.co.kr>
2023-08-24 09:00:22 +0900

    버그 발생 및 다시 1로 초기화

--- src/@pv/storage/pdf-storage.service.ts
+++ src/@pv/storage/pdf-storage.service.ts
@@ -42,7 +42,7 @@ export class PdfStorageService {
   */
   getViewportSize(pageNum) {
     // console.log(`> get ViewPort size: pageNum : ${pageNum}`);
-    return this._pdfVar.pdfPages[pageNum - 1].getViewport({ scale: 1.333 });
+    return this._pdfVar.pdfPages[pageNum - 1].getViewport({ scale: 1 });
   }
 
 
