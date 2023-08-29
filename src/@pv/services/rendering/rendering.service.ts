217ffdc60f135ddc8d1ce2d73ca35e27b2172a3d
limhokyun <lim@nsmarts.co.kr>
2023-08-29 13:00:22 +0900

    프로필 랜더링 스케일 0.5 -> 0.3으로 변경

--- src/@pv/services/rendering/rendering.service.ts
+++ src/@pv/services/rendering/rendering.service.ts
@@ -39,7 +39,7 @@ export class RenderingService {
     const tmpCtx = tmpCanvas.getContext("2d");
 
     // 1/2 scale로 설정 (임시)
-    const viewport = pdfPage.getViewport({ scale: 0.5 });
+    const viewport = pdfPage.getViewport({ scale: 0.3 });
     tmpCanvas.width = viewport.width;
     tmpCanvas.height = viewport.height;
 
@@ -166,60 +166,60 @@ export class RenderingService {
   }
 
 
-    /**
-   * 1. Thumbnail의 보드 rendering
-   *
-   * @param {element} thumbCanvas thumbnail canvas element
-   * @param {number} pageNum 페이지 번호
-   * @param {Object} data drawing data (tool, timediff, points)
+  /**
+ * 1. Thumbnail의 보드 rendering
+ *
+ * @param {element} thumbCanvas thumbnail canvas element
+ * @param {number} pageNum 페이지 번호
+ * @param {Object} data drawing data (tool, timediff, points)
+ */
+  renderThumbBoard(thumbCanvas, pageNum) {
+    let drawingEvents = this.drawStorageService.getDrawingEvents(pageNum);
+
+    // 해당 page의 drawing 정보가 있는 경우
+    if (drawingEvents?.drawingEvent && drawingEvents?.drawingEvent.length > 0) {
+
+      const viewport = CANVAS_CONFIG.fullSize;
+      const scale = thumbCanvas.width / (viewport.width);
+
+      const thumbCtx = thumbCanvas.getContext('2d');
+
+      thumbCtx.clearRect(0, 0, thumbCanvas.width, thumbCanvas.height);
+      thumbCtx.save();
+      thumbCtx.scale(scale, scale);
+
+      // Draw Service의 'end'관련 event 이용 전체 redraw
+      for (const item of drawingEvents?.drawingEvent) {
+        this.drawingService.end(thumbCtx, item.points, item.tool);
+      }
+      thumbCtx.restore();
+    }
+  }
+
+
+  /**
+   * Teacher Canvas의 board rendering
+   * @param {element} targetCanvas canvas element
+   * @param {number} zoomScale zoomScale
+   * @param {Object} drawingEvents 판서 event (tool, points, timeDiff)
    */
-    renderThumbBoard(thumbCanvas, pageNum) {
-      let drawingEvents = this.drawStorageService.getDrawingEvents(pageNum);
+  renderBoard(targetCanvas, zoomScale, drawingEvents) {
+    const targetCtx = targetCanvas.getContext('2d');
+    const scale = zoomScale || 1;
+    targetCtx.clearRect(0, 0, targetCanvas.width / scale, targetCanvas.height / scale);
+    console.log(targetCanvas.width, scale)
+    /*----------------------------------------
+      해당 page의 drawing 정보가 있는 경우
+      drawing Service의 'end'관련 event 이용.
+    -----------------------------------------*/
 
-      // 해당 page의 drawing 정보가 있는 경우
-      if (drawingEvents?.drawingEvent && drawingEvents?.drawingEvent.length > 0) {
-
-        const viewport = CANVAS_CONFIG.fullSize;
-        const scale = thumbCanvas.width / (viewport.width);
-
-        const thumbCtx = thumbCanvas.getContext('2d');
-
-        thumbCtx.clearRect(0, 0, thumbCanvas.width, thumbCanvas.height);
-        thumbCtx.save();
-        thumbCtx.scale(scale, scale);
-
-        // Draw Service의 'end'관련 event 이용 전체 redraw
-        for (const item of drawingEvents?.drawingEvent) {
-          this.drawingService.end(thumbCtx, item.points, item.tool);
-        }
-        thumbCtx.restore();
+    // 전체 redraw
+    if (drawingEvents?.drawingEvent && drawingEvents?.drawingEvent.length > 0) {
+      for (const item of drawingEvents?.drawingEvent) {
+        this.drawingService.end(targetCtx, item.points, item.tool);
       }
     }
-
-
-    /**
-     * Teacher Canvas의 board rendering
-     * @param {element} targetCanvas canvas element
-     * @param {number} zoomScale zoomScale
-     * @param {Object} drawingEvents 판서 event (tool, points, timeDiff)
-     */
-    renderBoard(targetCanvas, zoomScale, drawingEvents) {
-      const targetCtx = targetCanvas.getContext('2d');
-      const scale = zoomScale || 1;
-      targetCtx.clearRect(0, 0, targetCanvas.width / scale, targetCanvas.height / scale);
-      console.log(targetCanvas.width, scale)
-      /*----------------------------------------
-        해당 page의 drawing 정보가 있는 경우
-        drawing Service의 'end'관련 event 이용.
-      -----------------------------------------*/
-
-      // 전체 redraw
-      if (drawingEvents?.drawingEvent && drawingEvents?.drawingEvent.length > 0) {
-        for (const item of drawingEvents?.drawingEvent) {
-          this.drawingService.end(targetCtx, item.points, item.tool);
-        }
-      }
-    }
+  }
 
 
 }
