9af28b06379540e4d954bf202615f8a1abb577a8
jsdevper <outhkl9716@outlook.com>
2023-11-02 17:50:00 +0900

    [update] initcanvas

--- src/@pv/services/event-bus-render.service.ts
+++ src/@pv/services/event-bus-render.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class EventBusRenderService {
 
   constructor() { }
+
+
+  initCanvasSet() {
+
+
+    // canvas Element 할당
+    this.coverCanvas = this.coverCanvasRef.nativeElement;
+    this.teacherCanvas = this.teacherCanvasRef.nativeElement;
+    this.bgCanvas = this.bgCanvasRef.nativeElement;
+    this.tmpCanvas = this.tmpCanvasRef.nativeElement;
+    this.canvasContainer = this.canvasContainerRef.nativeElement;
+
+    /* container size 설정 */
+    CANVAS_CONFIG.maxContainerHeight = window.innerHeight - CANVAS_CONFIG.navbarHeight;; // pdf 불러오기 사이즈
+    CANVAS_CONFIG.maxContainerWidth = window.innerWidth - CANVAS_CONFIG.sidebarWidth;
+
+    CANVAS_CONFIG.deviceScale = this.canvasService.getDeviceScale(this.bgCanvas);
+  }
+
 }
