6b06df7ad1ffaee4246c0496c4145d9290416754
jsdevper <outhkl9716@outlook.com>
2023-11-08 13:13:13 +0900

    [update] set scale

--- src/@pv/services/event-bus-render.service.ts
+++ src/@pv/services/event-bus-render.service.ts
@@ -24,5 +24,58 @@ export class EventBusRenderService {
 
     CANVAS_CONFIG.deviceScale = this.canvasService.getDeviceScale(this.bgCanvas);
   }
+  etInitZoomScale() {
+    console.log('>>> Calc init Zoom scale...');
 
+    const containerSize = {
+      width: CANVAS_CONFIG.maxContainerWidth,
+      height: CANVAS_CONFIG.maxContainerHeight
+    };
+    const pdfPage: any = this.pdfStorageService.getPdfPage(1);
+
+
+    // console.log(pdfPage)
+    const docSize = pdfPage.getViewport({ scale: 1 * CANVAS_CONFIG.CSS_UNIT }); // 100%에 해당하는 document의 size (Css 기준)
+
+    // console.log(docSize);
+    // console.log(containerSize);
+
+    const ratio = {
+      w: containerSize.width / docSize.width,
+      h: containerSize.height / docSize.height
+    };
+
+    // console.log(ratio)
+
+    let zoomScale = 1;
+
+    // 1. main container size보다 작은 경우
+    if (ratio.w >= 1 && ratio.h >= 1) {
+      // console.log(' - 문서가 container보다 작음.');
+      // fit To page
+      zoomScale = Math.min(ratio.w, ratio.h);
+    }
+
+    // 2. landscape 문서인 경우
+    else if (docSize.width > docSize.height) {
+      // console.log(' - 문서: Landscape');
+      // fit To Page
+      zoomScale = Math.min(ratio.w, ratio.h);
+    }
+    // 3, portrait 문서인 경우
+    else if (docSize.width <= docSize.height) {
+      // console.log(' - 문서: Portrait');
+      if (ratio.w < 1) {
+        // console.log(' - 문서 Width가 container보다 넓습니다.');
+        zoomScale = ratio.w;
+      }
+    }
+
+    zoomScale = Math.min(zoomScale, this.maxZoomScale);
+    zoomScale = Math.max(zoomScale, this.minZoomScale);
+
+    console.log(' - Init zoom Scale : ', zoomScale);
+
+    return zoomScale;
+  }
 }
