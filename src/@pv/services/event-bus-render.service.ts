a85427271107262ff791be3724474dcbe3366fd7
jsdevper <outhkl9716@outlook.com>
2023-11-30 13:13:13 +0900

    [bug] socket

--- src/@pv/services/event-bus-render.service.ts
+++ src/@pv/services/event-bus-render.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EventBusRenderService {
-
+  private subject$ = new Subject();
   constructor() { }
   emit(event: EventData) {
     this.subject$.next(event);
