49c950d9d42d9de854e55475f85951aebe778d17
nsmarts-jmh <mcho@nsmarts.co.kr>
2023-12-26 13:25:14 +0900

    [Update] add annotations

--- src/@pv/store/store.ts
+++ src/@pv/store/store.ts
@@ -1,19 +1,23 @@
 import { Observable, BehaviorSubject } from 'rxjs';
 
+/**
+ * 상태관리 클래스.
+ * 컴포넌트 간 상태(변수) 공유
+ */
 export class Store<T> {
-	state$: Observable<T>;
-	private _state$: BehaviorSubject<T>;
+  state$: Observable<T>;
+  private _state$: BehaviorSubject<T>;
 
-	protected constructor(initialState: T) {
-		this._state$ = new BehaviorSubject(initialState);
-		this.state$ = this._state$.asObservable();
-	}
+  protected constructor(initialState: T) {
+    this._state$ = new BehaviorSubject(initialState);
+    this.state$ = this._state$.asObservable();
+  }
 
-	get state(): T {
-		return this._state$.getValue();
-	}
+  get state(): T {
+    return this._state$.getValue();
+  }
 
-	setState(nextState: T): void {
-		this._state$.next(nextState);
-	}
+  setState(nextState: T): void {
+    this._state$.next(nextState);
+  }
 }
